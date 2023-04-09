80f238cd6e9728b683d52d006412d685a772968d
Darlan Mendes <dedsondarlan@gmail.com>
2023-04-09 16:43:47 -0300

    correcao props interface cadastro aluno

--- src/pages/cadastroaluno/index.tsx
+++ src/pages/cadastroaluno/index.tsx
@@ -2,9 +2,15 @@ import {useState} from 'react';
 import NavBar from '@/components/NavBar';
 import MenuHamburguer from '@/components/MenuHamburguer';
 import styles from './styles.module.scss';
-import {MongoDB} from '../../lib/MongoDB';
-import {getSession} from 'next-auth/react'
-export default function CadastroAluno(props){
+
+
+interface props{
+    role:string,
+    
+}
+
+
+export default function CadastroAluno(props:props){
     const[isActived, setIsActived]= useState(false)
     return(
         <div>
@@ -33,19 +39,17 @@ export async function getServerSideProps(context:any) {
         
     }
     
-    const db = await MongoDB()
-    const data = await db.collection('livros').find().toArray()
+   
     
     if(session.user?.email?.includes('@prof.ce.gov.br')){
         return {props:{
-            role:'professor',
-            livros: JSON.parse(JSON.stringify(data))
+            role:'professor'
         }}
     }
     if(session.user?.email?.includes('@aluno.ce.gov.br')){
         return {props:{
             role:'aluno',
-            livros: JSON.parse(JSON.stringify(data))
+            
         }}
     }
     return{
